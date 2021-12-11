6f198dd82d141eb4e5f21cb0c4bc01984a031614
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-12-11 11:01:37 +0530

    Create bubbleSort.cpp

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,40 @@
+// https://www.geeksforgeeks.org/bubble-sort/
+
+#include <iostream>
+using namespace std;
+
+// this function will swap two variables
+void swap(int *a, int *b){
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+	// your code goes here
+	cout << "Bubble Sort" << endl;
+
+	// array to sort and its length in n
+	int a[5] = {8,5,7,2,1};
+	int n = 5;
+
+	// in bubble sort we compare two adjacent elements
+	// and swap them if they are not sorted
+	// in 1st loop we have no. of passes
+	// in each pass one element will always be in sorted position 
+	// that will be our last element
+	// the inner loop only runs till n - i - 1 as nth element is sorted after each pass
+	for (int i = 0; i < n - 1; i++){
+		for (int j = 0; j < n - i - 1; j++){
+
+			// sorting 
+			if (a[j] > a[j+1])
+				swap(&a[j], &a[j+1]);
+		}
+	}
+	
+	// printing
+	for (int i = 0; i < n; i++)
+	cout<<a[i]<<endl;
+}
