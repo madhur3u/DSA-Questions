1ddf42fcc94da065c67d16715b6f95153744d8a2
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2022-12-05 08:40:30 +0530

    convert dfs to bottom up approach

---
+++ Dynamic Programming/Frog Jump/tabulation.cpp
@@ -0,0 +1,23 @@
+// https://www.codingninjas.com/codestudio/problems/frog-jump_3621012
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &h) {
+    
+    vector<int> dp(n);
+    dp[0] = 0;            // if (i == 0) return 0; base case
+    
+    // bottom up approach, we took base case first now will go from 1 till top -> n
+    for (int i = 1; i < n; i++) {
+        
+        int jump1, jump2 = INT_MAX;
+        
+        // jump 1, take 1 step and add height diff in 1 step
+        jump1 = dp[i - 1] + abs(h[i] - h[i - 1]);
+
+        // jump 2 only when index > 1 as we dont want to take -1 case when i = 1
+        // add height diff of 2 jumps using heights array h
+        if (i > 1) jump2 = dp[i - 2] + abs(h[i] - h[i - 2]);
+        
+        dp[i] = min(jump1, jump2);
+    }
+    return dp[n - 1];
+}
